27800548c956aa960defdea5c856f8cf67ac9670
Rhodan Cervantes <105742277+rhodanrsc@users.noreply.github.com>
2023-05-09 11:42:57 -0600

    Minor styling

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -2,11 +2,19 @@ import React from "react";
 
 const AuthPage = () => {
     return(
-        <div className="loginbox">
-            <span className="title">Welcome!</span>
-            <span className="subtitle">Enter a username to start chatting!</span>
-            <input className="user" type="text" />
-            <button className="submit" type="submit">Enter</button>
+        <div className="authpage">
+            <div className="loginbox">
+                <div className="welcome">
+                    <span className="title">Welcome!</span>
+                    <br/>
+                    <span className="subtitle">Enter a username to start chatting!</span>
+                </div>
+                
+                <div className="form">
+                    <input className="user" type="text" />
+                    <button className="submit" type="submit">=&gt;</button>
+                </div>
+            </div>
         </div>
     );
 }
